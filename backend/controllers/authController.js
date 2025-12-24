4fdfdafcb77fca351baa2fc374afd3f4743c25b4
Aryan Dumale <aryandumale04@gmail.com>
2025-12-24 19:20:50 +0530

    Update try catch logic in auth controller

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -86,7 +86,6 @@ const loginUser = async(req, res) => {
 // @access Private (Requires JWT)
 const getUserProfile = async(req, res) => {
     try{
-
         const user = await  User.findById(req.user.id).select("-password");
         if( !user ){
             return res.status(404),json({message: "User not found!"});
