70e740bbd42e5a2bc2524772a9009f236a5d6378
Aryan Dumale <aryandumale04@gmail.com>
2026-01-05 23:16:18 +0530

    Add Questions Card with Animation

--- frontend/smartPrep/src/pages/InterviewPrep/InterviewPrep.jsx
+++ frontend/smartPrep/src/pages/InterviewPrep/InterviewPrep.jsx
@@ -7,6 +7,9 @@ import SpinnerLoader from '../../components/Loader/SpinnerLoader';
 import { toast } from "react-hot-toast";
 import DashboardLayout from '../../components/layouts/DashboardLayout';
 import RoleInfoHeader from './components/RoleInfoHeader';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import QuestionCard from "../../components/Cards/QuestionCard";
 
 const InterviewPrep = () => {
   const { sessionId } = useParams();
@@ -21,7 +24,18 @@ const InterviewPrep = () => {
   const [isUpdateLoader, setIsUpdateLoader] = useState(false);
 
   //Fetch Session data by session id
-  const fetchSessionDetailsById = async () =>  {} ;
+  const fetchSessionDetailsById = async () =>  {
+    try {
+      const response = await axiosInstance.get(
+        API_PATHS.SESSION.GET_ONE(sessionId)
+      );
+      if (response.data && response.data.session) {
+          setSessionData(response.data.session);
+      }
+    } catch (error) {
+      console.error("Error: ",error);
+    }
+  } ;
 
   //Generate Concept Explaantion
   const generateConceptExplanation = async (question) => {} ;
@@ -57,6 +71,49 @@ const InterviewPrep = () => {
           : ""
         }
          />
+         <div className='container mx-auto pt-4 pb-4 px-4 md:px-0'>
+          <h2 className='text-lg font-semibold color-black'>
+            Interview Q & A
+          </h2>
+          <div className='grid grid-cols-12 gap-4 mt-5 mb-10'>
+            <div className={`col-span-12 ${openLeanMoreDrawer ? "md:col-span-7" : "md-col-span-8"} `}>
+                <AnimatePresence>
+                  {sessionData?.questions?.map((data, index) => {
+                    return (
+                      <motion.div 
+                        key={data._id || index}
+                        initial={{ opacity: 0, y: -20}}
+                        animate={{ opacity: 1, y: 0}}
+                        exit={{ opacity: 0, scale: 0.95}}
+                        transition={{
+                          duration: 0.4,
+                          type: "spring",
+                          stiffness: 100,
+                          delay: index * 0.1,
+                          damping: 15,
+                        }}
+                        layout // This is the key prop that animates position changes
+                        layoutId={`question-${data._id || index}`} // Helps framer track speicific items
+                        >
+                          
+                            <>
+                            <QuestionCard
+                              question={data?.question}
+                              answer={data?.answer}
+                              onLearnMore={() =>
+                                generateConceptExplanation(data.question)
+                              }
+                              isPinned={data?.isPinned}
+                              onTogglePin={() => toggleQuestionPinStatus(data._id)}
+                              />
+                            </>
+                        </motion.div>
+                    );
+                  })}
+                </AnimatePresence>
+            </div>
+          </div>
+         </div>
     </DashboardLayout>
   )
 }
