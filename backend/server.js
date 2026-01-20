e9e2d95a4d894580f4958adb0ba78761b6d9a570
Aryan Dumale <aryandumale04@gmail.com>
2026-01-20 21:37:03 +0530

    configure CORS for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,45 +1,61 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const path = require("path")
-const  connectDB =  require("../backend/config/db");
+const path = require("path");
+const connectDB = require("../backend/config/db");
 const app = express();
 
 const authRoutes = require("./routes/authRoutes");
 const sessionRoutes = require("./routes/sessionRoutes");
-const questionRoutes = require("./routes/questionRoutes"); 
+const questionRoutes = require("./routes/questionRoutes");
 const { protect } = require("./middlewares/authMiddleware");
-const {generateInterviewQuestions, generateConceptExplanation} = require("./controllers/aiController")
+const {
+  generateInterviewQuestions,
+  generateConceptExplanation,
+} = require("./controllers/aiController");
 
+// CORS
+const allowedOrigins = [
+  process.env.CLIENT_URL,
+  "http://localhost:5173",
+  "http://localhost:3000",
+].filter(Boolean);
 
-//Middleware to handle CORS
 app.use(
-    cors({
-        origin: "*",
-        methods: ["GET","POST","PUT","DELETE"],
-        allowedHeaders: ["Content-Type","Authorization"]
-    })
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
 );
 
 connectDB();
-//Middleware 
+
+// Middleware
 app.use(express.json());
 
-
-//Routes
+// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/sessions", sessionRoutes);
-app.use("/api/questions",questionRoutes);
+app.use("/api/questions", questionRoutes);
 
 app.use("/api/ai/generate-questions", protect, generateInterviewQuestions);
 app.use("/api/ai/generate-explanation", protect, generateConceptExplanation);
 
+// Server uploads folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads"), {}));
 
-//Server uploads folder 
-app.use("/uploads",express.static(path.join(__dirname,"uploads"), {}));
-
-//Start Server
-const PORT = process.env.PORT  || 5000;
-app.listen(PORT, () =>{
-    console.log(`Server running on PORT : ${PORT}`);
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on PORT : ${PORT}`);
 });
